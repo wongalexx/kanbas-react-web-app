91432d9fb21be026fc4192e6ff876dac5643b7a5
al3xand3rw0ng <alexanderwong364@gmail.com>
2024-09-23 08:40:07 -0400

    Updated landing page

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,6 +8,7 @@ export default function TOC() {
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
+        <li id="wd-github"><a href="https://github.com/wongalexx/kanbas-react-web-app/tree/a1">GitHub</a></li>
       </ul>
     );
   }
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -6,6 +6,8 @@ import Lab3 from "./Lab3";
 export default function Labs() {
   return (
     <div>
+      <h1>Alexander Wong</h1>
+      <h1>Section 01</h1>
       <h1>Labs</h1>
       <TOC />
       <Routes>
