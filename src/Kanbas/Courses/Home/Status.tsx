8c1b90fb7f687c5ee711ce124c172dc933ea4b59
al3xand3rw0ng <alexanderwong364@gmail.com>
2024-10-01 18:39:41 -0400

    a2 implemented up to 4.3

--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -1,16 +1,40 @@
+import { MdDoNotDisturbAlt } from "react-icons/md";
+import { FaCheckCircle } from "react-icons/fa";
+import { BiImport } from "react-icons/bi";
+import { LiaFileImportSolid } from "react-icons/lia";
+import { ImTarget } from "react-icons/im";
+import { RiBarChart2Fill } from "react-icons/ri";
+import { GrAnnounce } from "react-icons/gr";
+import { IoNotificationsOutline } from "react-icons/io5";
+
 export default function CourseStatus() {
-    return (
-      <div id="wd-course-status">
-        <h2>Course Status</h2>
-        <button>Unpublish</button> <button>Publish</button><br />
-        <p></p>
-        <button>Import Existing Content</button><br />
-        <button>Import from Commons</button><br />
-        <button>Choose Home Page</button><br />
-        <button>View Course Stream</button><br />
-        <button>New Announcement</button><br />
-        <button>New Analytics</button><br />
-        <button>View Course Notifications</button><br />
-      </div>
-  );}
+  return (
+    <div id="wd-course-status" style={{ width: "300px" }}>
+      <h2>Course Status</h2>
+      <div className="d-flex">
+        <div className="w-50 pe-1">
+          <button className="btn btn-lg btn-secondary w-100 text-nowrap ">
+            <MdDoNotDisturbAlt className="me-2 fs-5" /> Unpublish </button>
+        </div>
+        <div className="w-50">
+          <button className="btn btn-lg btn-success w-100">
+            <FaCheckCircle className="me-2 fs-5" /> Publish </button>
+        </div>
+      </div><br />
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <BiImport className="me-2 fs-5" /> Import Existing Content </button>
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <LiaFileImportSolid className="me-2 fs-5" /> Import from Commons </button>
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <ImTarget className="me-2 fs-5" /> Choose Home Page </button>
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <RiBarChart2Fill className="me-2 fs-5" /> View Course Stream </button>
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <GrAnnounce className="me-2 fs-5" /> New Announcement </button>
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <RiBarChart2Fill className="me-2 fs-5" /> New Analytics </button>
+      <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+        <IoNotificationsOutline className="me-2 fs-5" /> View Course Notifications </button>
+    </div>
+);}
   
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -3,27 +3,23 @@ import Account from "./Account";
 import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
 import Courses from "./Courses";
+import "./styles.css";
 
 
 export default function Kanbas() {
   return (
     <div id="wd-kanbas">
-      <table>
-        <tr>
-          <td valign="top">
-            <KanbasNavigation />
-          </td>
-          <td valign="top">
+        <KanbasNavigation />
+        <div className="wd-main-content-offset p-3">
+          <div>
             <Routes>
               <Route path="/" element={<Navigate to="Account" />} />
               <Route path="/Account/*" element={<Account />} />
               <Route path="/Dashboard" element={<Dashboard />} />
               <Route path="/Courses/:cid/*" element={<Courses />} />
-              <Route path="/Calendar" element={<h1>Calendar</h1>} />
-              <Route path="/Inbox" element={<h1>Inbox</h1>} />
             </Routes>
-          </td>
-        </tr>
-      </table>
+          </div>
+        </div>
     </div>
-);}
+  );
+}
