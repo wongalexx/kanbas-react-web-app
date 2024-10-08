de1906a0ff20448188084f57e8fc8bc9f071e1af
al3xand3rw0ng <alexanderwong364@gmail.com>
2024-10-08 17:07:36 -0400

    Fixed margins on course home page

--- src/Kanbas/Courses/Home/index.tsx
+++ src/Kanbas/Courses/Home/index.tsx
@@ -3,7 +3,7 @@ import CourseStatus from "./Status";
 export default function Home() {
   return (
     <div className="d-flex" id="wd-home">
-      <div className="flex-fill">
+      <div className="flex-fill me-2">
         <Modules />
       </div>
       <div className="d-none d-md-block">
