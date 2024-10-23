0814525983bcb7b8239a7c4759e88cace70e6dd9
al3xand3rw0ng <alexanderwong364@gmail.com>
2024-10-23 12:18:51 -0400

    completed labs for A4

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -32,12 +32,23 @@ import Square from "./Square";
 import Highlight from "./Highlight";
 import AddPathParameters from "./AddPathParameters";
 import PathParameters from "./PathParameters";
+import { useSelector } from "react-redux";
 export default function Lab3() {
   console.log('Hello World!');
+  const { todos } = useSelector((state: any) => state.todosReducer);
   return (
     <div id="wd-lab3">
       <h3>Lab 3</h3>
       <h3>JavaScript</h3>
+      <hr />
+      <ul className="list-group">
+        {todos.map((todo: any) => (
+          <li className="list-group-item" key={todo.id}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+      <hr />
       <VariablesAndConstants />
       <VariableTypes />
       <BooleanVariables />
