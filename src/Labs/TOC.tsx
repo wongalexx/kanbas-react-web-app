81de0a2e01d27b1ba854ec3f87937cf175a9700f
al3xand3rw0ng <alexanderwong364@gmail.com>
2024-11-05 12:59:41 -0500

    a4 Redux

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -9,26 +9,38 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-a1" href="#/Labs/Lab1"
-          className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}>
+        <a
+          id="wd-a1"
+          href="#/Labs/Lab1"
+          className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}
+        >
           Lab 1
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-a2" href="#/Labs/Lab2"
-          className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>
+        <a
+          id="wd-a2"
+          href="#/Labs/Lab2"
+          className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}
+        >
           Lab 2
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-a3" href="#/Labs/Lab3"
-          className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>
+        <a
+          id="wd-a3"
+          href="#/Labs/Lab3"
+          className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}
+        >
           Lab 3
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-a4" href="#/Labs/Lab4"
-          className={`nav-link ${pathname.includes("Lab4") ? "active" : ""}`}>
+        <a
+          id="wd-a4"
+          href="#/Labs/Lab4"
+          className={`nav-link ${pathname.includes("Lab4") ? "active" : ""}`}
+        >
           Lab 4
         </a>
       </li>
@@ -38,7 +50,11 @@ export default function TOC() {
         </a>
       </li>
       <li className="wd-github">
-        <a id="wd-k" href="https://github.com/wongalexx/kanbas-react-web-app/tree/a3" className="nav-link">
+        <a
+          id="wd-k"
+          href="https://github.com/wongalexx/kanbas-react-web-app/tree/a4"
+          className="nav-link"
+        >
           My GitHub
         </a>
       </li>
