2624149b1b7045f53ab59abcb474cd8a6d90caa7
al3xand3rw0ng <alexanderwong364@gmail.com>
2024-11-18 21:51:23 -0500

    halfway through labs

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,11 +1,12 @@
 import Lab1 from "./Lab1";
 import { Route, Routes, Navigate } from "react-router";
-import TOC from "./TOC"
+import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
 import Lab4 from "./Lab4";
 import store from "./store";
 import { Provider } from "react-redux";
+import Lab5 from "./Lab5";
 export default function Labs() {
   return (
     <Provider store={store}>
@@ -20,6 +21,7 @@ export default function Labs() {
           <Route path="Lab2" element={<Lab2 />} />
           <Route path="Lab3/*" element={<Lab3 />} />
           <Route path="Lab4" element={<Lab4 />} />
+          <Route path="Lab5" element={<Lab5 />} />
         </Routes>
       </div>
     </Provider>
