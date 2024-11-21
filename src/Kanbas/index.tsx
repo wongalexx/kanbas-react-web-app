274884a869306cc91d0b2b213e2e995330059e0c
al3xand3rw0ng <alexanderwong364@gmail.com>
2024-11-21 18:12:06 -0500

    moved it back

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -43,44 +43,46 @@ export default function Kanbas() {
     );
   };
   return (
-    <Session>
-      <div id="wd-kanbas">
-        <KanbasNavigation />
-        <div className="wd-main-content-offset p-3">
-          <Routes>
-            <Route path="/" element={<Navigate to="Dashboard" />} />
-            <Route path="Account/*" element={<Account />} />
-            <Route
-              path="Dashboard"
-              element={
-                <ProtectedRoute>
-                  <Dashboard
-                    courses={courses}
-                    setCourses={setCourses}
-                    course={course}
-                    setCourse={setCourse}
-                    addNewCourse={addNewCourse}
-                    deleteCourse={deleteCourse}
-                    updateCourse={updateCourse}
-                  />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="Courses/:cid/*"
-              element={
-                <ProtectedRoute>
-                  <ProtectedCoursesRoute>
-                    <Courses courses={courses} />
-                  </ProtectedCoursesRoute>
-                </ProtectedRoute>
-              }
-            />
-            <Route path="Calendar" element={<h1>Calendar</h1>} />
-            <Route path="Inbox" element={<h1>Inbox</h1>} />
-          </Routes>
+    <Provider store={store}>
+      <Session>
+        <div id="wd-kanbas">
+          <KanbasNavigation />
+          <div className="wd-main-content-offset p-3">
+            <Routes>
+              <Route path="/" element={<Navigate to="Dashboard" />} />
+              <Route path="Account/*" element={<Account />} />
+              <Route
+                path="Dashboard"
+                element={
+                  <ProtectedRoute>
+                    <Dashboard
+                      courses={courses}
+                      setCourses={setCourses}
+                      course={course}
+                      setCourse={setCourse}
+                      addNewCourse={addNewCourse}
+                      deleteCourse={deleteCourse}
+                      updateCourse={updateCourse}
+                    />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="Courses/:cid/*"
+                element={
+                  <ProtectedRoute>
+                    <ProtectedCoursesRoute>
+                      <Courses courses={courses} />
+                    </ProtectedCoursesRoute>
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="Calendar" element={<h1>Calendar</h1>} />
+              <Route path="Inbox" element={<h1>Inbox</h1>} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Session>
+      </Session>
+    </Provider>
   );
 }
